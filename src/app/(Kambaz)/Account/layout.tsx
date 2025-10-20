494d3dca7f5c702359f34a61fe09a1f4c816adc1
PaarthKulkarni <85743897+PaarthKulkarni@users.noreply.github.com>
2025-10-20 13:08:27 -0400

    Update layout.tsx

--- src/app/(Kambaz)/Account/layout.tsx
+++ src/app/(Kambaz)/Account/layout.tsx
@@ -4,7 +4,7 @@ export default function AccountLayout({ children }: Readonly<{ children: ReactNo
  return (
    <div id="wd-kambaz">
          <div className="d-flex">
-           <div style ={{ width: '250px', borderRight: '1px solid #ccc' , minWidth: '200px'}}>
+           <div className="d-none d-md-block">
              <AccountNavigation />
             </div>
            <div className="flex-fill">

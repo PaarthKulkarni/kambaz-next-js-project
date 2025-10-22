16108fd4ed5652546c7d71f3f1363a10f462b77f
Paarth <paarthkiran1996@gmail.com>
2025-10-21 20:43:04 -0400

    Added Breadcrumb

--- src/app/Labs/Lab3/FindFunction.tsx
+++ src/app/Labs/Lab3/FindFunction.tsx
@@ -3,10 +3,15 @@ export default function FindFunction() {
   let stringArray1 = ["string1", "string2", "string3"];
   const four = numberArray1.find((a) => a === 4);
   const string3 = stringArray1.find((a) => a === "string3");
+
+const fourIndex = numberArray1.findIndex(a => a === 4);
+const string3Index = stringArray1.findIndex(a => a === 'string3');
   return (
     <div id="wd-find-function">
       <h4>Find Function</h4>
       four = {four} <br />
-      string3 = {string3} <hr />
+      string3 = {string3} <br />
+      fourIndex = {fourIndex} <br />
+      string3Index = {string3Index} <hr/>
     </div>
 );}

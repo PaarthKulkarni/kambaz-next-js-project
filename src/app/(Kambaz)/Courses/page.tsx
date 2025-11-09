39afe9f15c5b63b43991dd5da8d2bf2469b018a2
Paarth <paarthkiran1996@gmail.com>
2025-11-09 18:22:00 -0500

    Fixed Typos

--- src/app/(Kambaz)/Courses/page.tsx
+++ src/app/(Kambaz)/Courses/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import Link from "next/link";
 import * as db from "../Database";
@@ -7,17 +7,26 @@ import { RootState } from "../store";
 import { CardBody, Row, Col, Card, CardImg, CardTitle, CardText, Button, FormControl } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
 import { addNewCourse, deleteCourse, updateCourse } from "../Courses/reducer";
+import { addEnrollment, deleteEnrollment } from "../enrollmentReducer";
 export default function Courses() {
+  const [showAllCourses, setShowAllCourses] = useState(false);
   const { courses } = useSelector(
     (state: RootState) => state.coursesReducer
   );
   const { currentUser } = useSelector((state: RootState) => state.accountReducer);
-  const { enrollments } = db;
+  const { enrollments } = useSelector(
+    (state: RootState) => state.enrollmentReducer
+  );
   const [course, setCourse] = useState<any>({
     _id: "0", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
     image: "/images/reactjs.jpg", description: "New Description"
   });
+
+  const isEnrolled = (courseId: string) => {
+    if (!currentUser) return false;
+    return enrollments.some((e) => e.user === currentUser._id && e.course === courseId);
+  };
 
   const dispatch = useDispatch();
   const isFaculty = currentUser?.role === "FACULTY";
@@ -32,15 +41,25 @@ export default function Courses() {
     dispatch(updateCourse(course));
   };
 
+  const handleEnroll = (courseId: string) => {
+    dispatch(addEnrollment({userId: currentUser._id, courseId}))
+  }
 
+  const handleUnenroll = (courseId: string) => {
+    dispatch(deleteEnrollment({userId: currentUser._id, courseId}))
+  }
 
+  const coursesDisplay = showAllCourses ? courses : courses.filter((course) => isEnrolled(course._id));
   return (
     <div id="wd-dashboard" style={{ marginLeft: 130, marginRight: 20 }}>
       <h1 id="wd-dashboard-title">Courses</h1> <hr />
+      <button className="btn btn-primary float-end" id="wd-add-new-enrollment-click" onClick={() => setShowAllCourses(!showAllCourses)}>
+        {showAllCourses ? "Enrollments" : "Available"}
+      </button>
       {isFaculty && (
         <>
             <h5>New Course
-          <button className="btn btn-primary float-end"
+          <button className="btn btn-primary float-end me-2"
                   id="wd-add-new-course-click"
                   onClick={handleAddNewCourse} > Add </button>
                   <button className="btn btn-warning float-end me-2"
@@ -52,18 +71,13 @@ export default function Courses() {
       <FormControl as = "textarea" value={course.description} rows={3} onChange={(e) => setCourse({ ...course, description: e.target.value }) } />
       <hr />
       </>)}
-      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
+      <h2 id="wd-dashboard-published">{showAllCourses ? "Published Courses" : "Enrolled Courses"} ({coursesDisplay.length})</h2> <hr />
       <div id="wd-dashboard-courses">
         <Row xs={1} md={5} className="g-4">
-        {courses.filter((course) => currentUser &&
-      enrollments.some(
-        (enrollment) =>
-          enrollment.user === currentUser._id &&
-          enrollment.course === course._id
-         ))
-        .map((course) => (
+        {coursesDisplay.map((course) => (
         <Col key={course._id} className="wd-dashboard-course" style={{ width: "300px" }}>
         <Card>
+          {!showAllCourses && (<>
           <Link href={`/Courses/${course._id}/Home`} className="wd-dashboard-course-link text-decoration-none text-dark">
             <CardImg variant="top" src="/images/reactjs.jpg" alt="React JS" width="100%" height={160} />
             <CardBody>
@@ -91,6 +105,33 @@ export default function Courses() {
     </>)}
             </CardBody>
           </Link>
+          </>)}
+          {showAllCourses && (
+            <>
+            <CardImg variant="top" src="/images/reactjs.jpg" alt="React JS" width="100%" height={160} />
+            <CardBody>
+              <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">{course.name}</CardTitle>
+              <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                {course.description}
+              </CardText>
+              {isEnrolled(course._id) ? (
+                          <button
+                            onClick={(e) => handleUnenroll(course._id)}
+                            className="btn btn-danger float-end"
+                          >
+                            Unenroll
+                          </button>
+                        ) : (
+                          <button
+                            onClick={(e) => handleEnroll(course._id)}
+                            className="btn btn-success float-end"
+                          >
+                            Enroll
+                          </button>
+                        )}
+              </CardBody>
+              </>
+          )}
           </Card>
         </Col>
         ))}
